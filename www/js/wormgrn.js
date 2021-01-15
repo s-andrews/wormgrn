506bbe7599d8931936d2cbbb99c27446600eda62
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-15 17:11:16 +0000

    Make edge weight filter work.

--- www/js/wormgrn.js
+++ www/js/wormgrn.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $("#search").prop("disabled",true);
 
-    network = "max_pfe"
+    network = "max_aufe"
     // network = "small"
 
     colour_brewer = ["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999"];
@@ -28,44 +28,76 @@ $(document).ready(function(){
         var elements = [];
         var node_names = {};
         var min_weight = 0;
-        var max_weight = 0;
+        var mean_weight = 0;
+        var edge_weight_count = 0;
 
         node_names[gene] = 1;
+
+        var node_counts = 1;
+
+
+        // We need to know the edge weight range.  Eventually
+        // we should calculate this at the point we load the
+        // data but we'll kludge it here for now.
+        for (i=0;i<network_data.length;i++) {
+            if (network_data[i]["group"] == "edges") {
+                if (min_weight = 0 | network_data[i]["data"]["weight"] < min_weight) {
+                    min_weight = network_data[i]["data"]["weight"];
+                }
+
+                mean_weight += network_data[i]["data"]["weight"];
+                edge_weight_count++;
+            }
+        }
+
+        mean_weight /= edge_weight_count;
+        mean_weight = mean_weight*10;
+
+        // Now we can work out the weight cutoff from the position of the slider
+        var weight_percent = $("#weight").val();
+        var weight_cutoff = (((mean_weight - min_weight)/100)*weight_percent)+min_weight;
+
+        console.log("Weight cutoff is "+weight_cutoff+" from "+min_weight+" , "+mean_weight+" , "+weight_percent);
 
         // Go through the nodes.  We need all edges mentioning
         // this search
         for (i=0;i<network_data.length;i++) {
             if (network_data[i]["group"] == "edges") {
+
+                if (network_data[i]["data"]["weight"] < weight_cutoff) {
+                    continue;
+                }
+
                 if (network_data[i]["data"]["source"]==gene) {
                     elements.push(network_data[i]);
-
-                    if (min_weight = 0 | Math.abs(network_data[i]["data"]["weight"]) < min_weight) {
-                        min_weight = Math.abs(network_data[i]["data"]["weight"]);
-                    }
-                    if (Math.abs(network_data[i]["data"]["weight"]) > max_weight) {
-                        max_weight = Math.abs(network_data[i]["data"]["weight"]);
-                    }
-
                     node_names[network_data[i]["data"]["target"]] = 1;
+                    node_counts++;
                 }
                 else if (network_data[i]["data"]["target"]==gene) {
                     elements.push(network_data[i]);
-
-                    if (min_weight = 0 | Math.abs(network_data[i]["data"]["weight"]) < min_weight) {
-                        min_weight = Math.abs(network_data[i]["data"]["weight"]);
-                    }
-                    if (Math.abs(network_data[i]["data"]["weight"]) > max_weight) {
-                        max_weight = Math.abs(network_data[i]["data"]["weight"]);
-                    }
-
-
                     node_names[network_data[i]["data"]["source"]] = 1;
+                    node_counts++;
                 }
             }
         }
 
-        // Now do a second pass looking for the nodes we need.
+        if (node_counts > 100) {
+            // TODO: Put up an error
+            console.log("Not drawing "+gene+" as "+node_counts+" nodes found");
+            return;
+        }
+
+
+        // Now do a second pass looking for the nodes we need and any additional
+        // edges amongst the additional nodes.
         for (i=0;i<network_data.length;i++) {
+
+            if (network_data[i]["group"] == "edges" & !(network_data[i]["data"]["source"] == gene | network_data[i]["data"]["target"]=="gene")) {
+                if (network_data[i]["data"]["source"] in node_names & network_data[i]["data"]["target"] in node_names) {
+                    elements.push(network_data[i]);
+                }
+            }
+
             if (network_data[i]["group"] == "nodes") {
                 if (network_data[i]["data"]["id"] in node_names) {
                     elements.unshift(network_data[i]);
@@ -73,7 +105,7 @@ $(document).ready(function(){
             }
         }
 
-        updateGraph(elements, min_weight, max_weight);
+        updateGraph(elements, min_weight, mean_weight);
 
     });
 
@@ -109,8 +141,8 @@ function updateGraph (data, min_weight, max_weight) {
         ],
         
         layout: {
-            name: 'grid',
-            randomize: false,
+            name: "cose-bilkent",
+            randomize: true,
             animate: false, 
             idealEdgeLength: 250,
             nodeRepulsion: 2048
