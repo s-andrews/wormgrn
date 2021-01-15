8a6172262d19755a88fe75197008d939f557ac96
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-15 20:19:46 +0000

    Search fixes

--- www/js/wormgrn.js
+++ www/js/wormgrn.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
 
-    current_network = "";
+    current_network = "max_aufe";
     min_weight = 0;
     mean_weight = 0;
 
+    $("#search").prop("disabled",true);
+    loadNetwork();
+
     colour_brewer = ["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999"];
+
+    gene_suggestions = []
+
+    $('#genename').autoComplete({
+        resolver:'custom',
+        minLength: 1,
+        events: {
+            search: function(qry, callback) {
+                console.log("Autocomplete running for "+qry);
+                suggestions = []
+                for (i in gene_suggestions) {
+                    var thisName = gene_suggestions[i]
+                    if (thisName.toLowerCase().indexOf(qry.toLowerCase()) > -1) {
+                        suggestions.push(thisName)
+                        if (suggestions.length==100) {
+                            break;
+                        }
+                    }
+                }
+                callback(suggestions)
+            }
+        }
+    });
+
 
     // Make the graph export work
     // $('#export').click(function() {
@@ -26,7 +53,6 @@ function run_search() {
     // Make the search button inactive
     $("#search").prop("disabled",true);
 
-
     // See if we need to update the loaded network
     var selected_network = $("#nettype").val();
 
@@ -38,9 +64,11 @@ function run_search() {
 
     // Get the gene name from the search
     var gene = $("#genename").val()
+    // console.log("Search term is "+gene);
 
 
     if (gene.length==0) {
+        $("#search").prop("disabled",false);
         return;
     }
 
@@ -56,7 +84,7 @@ function run_search() {
     var weight_percent = $("#weight").val();
     var weight_cutoff = (((mean_weight - min_weight)/100)*weight_percent)+min_weight;
 
-    console.log("Weight cutoff is "+weight_cutoff+" from "+min_weight+" , "+mean_weight+" , "+weight_percent);
+    // console.log("Weight cutoff is "+weight_cutoff+" from "+min_weight+" , "+mean_weight+" , "+weight_percent);
 
     // Go through the nodes.  We need all edges mentioning
     // this search
@@ -82,10 +110,10 @@ function run_search() {
         }
     }
 
-    console.log("Found "+node_counts+" for "+gene);
+    // console.log("Found "+node_counts+" for "+gene);
 
     if (!found_query) {
-        $("#warning").text("Couldn't find "+gene);
+        $("#warning").text("No interactions found");
         // Make the search button active
         $("#search").prop("disabled",false);
 
@@ -127,11 +155,13 @@ function run_search() {
 
 function loadNetwork () {
     $.getJSON("json/"+current_network+".json", function(data) {
-        console.log("Loaded "+current_network);
+        // console.log("Loaded "+current_network);
+        gene_suggestions = []
         network_data = data;
         var edge_weight_count = 0;
 
         // We need to know the edge weight range.  
+        // console.log("Iterating through elements")
         for (i=0;i<network_data.length;i++) {
             if (network_data[i]["group"] == "edges") {
                 if (min_weight = 0 | network_data[i]["data"]["weight"] < min_weight) {
@@ -141,7 +171,13 @@ function loadNetwork () {
                 mean_weight += network_data[i]["data"]["weight"];
                 edge_weight_count++;
             }
+            else {
+                // It's a node
+                gene_suggestions.push(network_data[i]["data"]["id"]);
+            }
         }
+
+        gene_suggestions.sort();
 
         mean_weight /= edge_weight_count;
         mean_weight = mean_weight*10;
