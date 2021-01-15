1d8c25291957f40ee6af91654d6b11f35b884faa
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-15 17:57:47 +0000

    Make switching networks work

--- www/js/wormgrn.js
+++ www/js/wormgrn.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 
-    $("#search").prop("disabled",true);
-
-    network = "max_aufe"
-    // network = "small"
+    current_network = "";
+    min_weight = 0;
+    mean_weight = 0;
 
     colour_brewer = ["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999"];
-
-    $.getJSON("json/"+network+".json", function(data) {$("#search").prop("disabled",false);network_data = data});
 
     // Make the graph export work
     // $('#export').click(function() {
@@ -17,32 +14,124 @@ $(document).ready(function(){
     // })
 
 
-    $("#search").click(function(){
-        // Get the gene name from the search
-        var gene = $("#genename").val()
+    $("#search").click(run_search);
 
-        // Clear any previous errors
-        $("#warning").text("");
+}); 
 
-        if (gene.length==0) {
-            return;
+function run_search() {
+
+    // Clear any previous errors
+    $("#warning").text("");
+
+    // Make the search button inactive
+    $("#search").prop("disabled",true);
+
+
+    // See if we need to update the loaded network
+    var selected_network = $("#nettype").val();
+
+    if (selected_network != current_network) {
+        current_network = selected_network;
+        loadNetwork();
+        return;
+    }
+
+    // Get the gene name from the search
+    var gene = $("#genename").val()
+
+
+    if (gene.length==0) {
+        return;
+    }
+
+    var elements = [];
+    var node_names = {};
+
+    node_names[gene] = 1;
+
+    var node_counts = 1;
+    var found_query = false;
+
+    // Now we can work out the weight cutoff from the position of the slider
+    var weight_percent = $("#weight").val();
+    var weight_cutoff = (((mean_weight - min_weight)/100)*weight_percent)+min_weight;
+
+    console.log("Weight cutoff is "+weight_cutoff+" from "+min_weight+" , "+mean_weight+" , "+weight_percent);
+
+    // Go through the nodes.  We need all edges mentioning
+    // this search
+    for (i=0;i<network_data.length;i++) {
+        if (network_data[i]["group"] == "edges") {
+
+            if (network_data[i]["data"]["weight"] < weight_cutoff) {
+                continue;
+            }
+
+            if (network_data[i]["data"]["source"]==gene) {
+                found_query = true;
+                elements.push(network_data[i]);
+                node_names[network_data[i]["data"]["target"]] = 1;
+                node_counts++;
+            }
+            else if (network_data[i]["data"]["target"]==gene) {
+                found_query = true;
+                elements.push(network_data[i]);
+                node_names[network_data[i]["data"]["source"]] = 1;
+                node_counts++;
+            }
+        }
+    }
+
+    console.log("Found "+node_counts+" for "+gene);
+
+    if (!found_query) {
+        $("#warning").text("Couldn't find "+gene);
+        // Make the search button active
+        $("#search").prop("disabled",false);
+
+        return;
+    }
+
+    if (node_counts > 100) {
+        $("#warning").text("Not drawing "+gene+" as "+node_counts+" nodes found");
+        // Make the search button active
+        $("#search").prop("disabled",false);
+        return;
+    }
+
+
+    // Now do a second pass looking for the nodes we need and any additional
+    // edges amongst the additional nodes.
+    for (i=0;i<network_data.length;i++) {
+
+        if (network_data[i]["group"] == "edges" & !(network_data[i]["data"]["source"] == gene | network_data[i]["data"]["target"]=="gene")) {
+            if (network_data[i]["data"]["source"] in node_names & network_data[i]["data"]["target"] in node_names) {
+                elements.push(network_data[i]);
+            }
         }
 
-        var elements = [];
-        var node_names = {};
-        var min_weight = 0;
-        var mean_weight = 0;
+        if (network_data[i]["group"] == "nodes") {
+            if (network_data[i]["data"]["id"] in node_names) {
+                elements.unshift(network_data[i]);
+            }
+        }
+    }
+
+    updateGraph(elements, min_weight, mean_weight);
+
+    // Make the search button active
+    $("#search").prop("disabled",false);
+
+
+}
+
+function loadNetwork () {
+    $.getJSON("json/"+current_network+".json", function(data) {
+        console.log("Loaded "+current_network);
+        network_data = data;
         var edge_weight_count = 0;
 
-        node_names[gene] = 1;
-
-        var node_counts = 1;
-        var found_query = false;
-
-
-        // We need to know the edge weight range.  Eventually
-        // we should calculate this at the point we load the
-        // data but we'll kludge it here for now.
+        // We need to know the edge weight range.  
         for (i=0;i<network_data.length;i++) {
             if (network_data[i]["group"] == "edges") {
                 if (min_weight = 0 | network_data[i]["data"]["weight"] < min_weight) {
@@ -57,71 +146,10 @@ $(document).ready(function(){
         mean_weight /= edge_weight_count;
         mean_weight = mean_weight*10;
 
-        // Now we can work out the weight cutoff from the position of the slider
-        var weight_percent = $("#weight").val();
-        var weight_cutoff = (((mean_weight - min_weight)/100)*weight_percent)+min_weight;
-
-        console.log("Weight cutoff is "+weight_cutoff+" from "+min_weight+" , "+mean_weight+" , "+weight_percent);
-
-        // Go through the nodes.  We need all edges mentioning
-        // this search
-        for (i=0;i<network_data.length;i++) {
-            if (network_data[i]["group"] == "edges") {
-
-                if (network_data[i]["data"]["weight"] < weight_cutoff) {
-                    continue;
-                }
-
-                if (network_data[i]["data"]["source"]==gene) {
-                    found_query = true;
-                    elements.push(network_data[i]);
-                    node_names[network_data[i]["data"]["target"]] = 1;
-                    node_counts++;
-                }
-                else if (network_data[i]["data"]["target"]==gene) {
-                    found_query = true;
-                    elements.push(network_data[i]);
-                    node_names[network_data[i]["data"]["source"]] = 1;
-                    node_counts++;
-                }
-            }
-        }
-
-        console.log("Found "+node_counts+" for "+gene);
-
-        if (!found_query) {
-            $("#warning").text("Couldn't find "+gene);
-            return;
-        }
-
-        if (node_counts > 100) {
-            $("#warning").text("Not drawing "+gene+" as "+node_counts+" nodes found");
-            return;
-        }
-
-
-        // Now do a second pass looking for the nodes we need and any additional
-        // edges amongst the additional nodes.
-        for (i=0;i<network_data.length;i++) {
-
-            if (network_data[i]["group"] == "edges" & !(network_data[i]["data"]["source"] == gene | network_data[i]["data"]["target"]=="gene")) {
-                if (network_data[i]["data"]["source"] in node_names & network_data[i]["data"]["target"] in node_names) {
-                    elements.push(network_data[i]);
-                }
-            }
-
-            if (network_data[i]["group"] == "nodes") {
-                if (network_data[i]["data"]["id"] in node_names) {
-                    elements.unshift(network_data[i]);
-                }
-            }
-        }
-
-        updateGraph(elements, min_weight, mean_weight);
+        run_search();
 
     });
-
-}); 
+}
 
 function updateGraph (data, min_weight, max_weight) {
     cy = cytoscape({
